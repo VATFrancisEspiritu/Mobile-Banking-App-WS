da5a1d5860ee4d94abcb3a7f577f50a3d8a06ca3
VATFrancisEspiritu <francis.espiritu@valtes.com.ph>
2017-01-20 17:27:49 +0800

    password encryption

--- src/main/java/controller/RegistrationController.java
+++ src/main/java/controller/RegistrationController.java
@@ -1,9 +1,12 @@
-package hello;
+package controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +18,16 @@ public class RegistrationController {
   private final String dbUrl = "jdbc:mysql://localhost/mobilebankingapp?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
   @RequestMapping("/registerUser")
-  public String getUserCount(@RequestParam final String pEMail, final String pFullname, final String pCCNumber, final String pMobileNumber, final String pPassword, final String pBirthdate) {
-    final String numusers = "";
+  public void getUserCount(@RequestParam final String pEMail, final String pFullname, final String pCCNumber, final String pMobileNumber, final String pPassword, final String pBirthdate) {
+
+    byte[] encrypted = null;
+
+    encrypted = encrypt(pPassword, "test key");
+
     final String insertQuery = "INSERT INTO user_details (EMAIL, NAME, CREDIT_CARD_NUMBER, PHONE_NUMBER, PASSWORD, BIRTHDATE, IS_VERIFIED)" + "VALUES ('" + pEMail + "', '" + pFullname + "', "
-        + pCCNumber + ", '" + pMobileNumber + "', '" + pPassword + "', '" + pBirthdate + "', 0)";
+        + pCCNumber + ", '" + pMobileNumber + "', '" + encrypted + "', '" + pBirthdate + "', 0)";
     final String userName = "root", password = "root";
+
     try {
 
       Class.forName("com.mysql.cj.jdbc.Driver");
@@ -33,7 +41,20 @@ public class RegistrationController {
       e.printStackTrace();
     } finally {
     }
-    return numusers;
+  }
 
+  public static byte[] encrypt(final String pPassword, final String pKey) {
+    byte[] encrypted = null;
+
+    try {
+      final SecretKeySpec skeyspec = new SecretKeySpec(pKey.getBytes(), "Blowfish");
+      final Cipher cipher = Cipher.getInstance("Blowfish");
+      cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
+      encrypted = cipher.doFinal(pPassword.getBytes());
+
+    } catch (final Exception e) {
+      e.printStackTrace();
+    }
+    return encrypted;
   }
 }
